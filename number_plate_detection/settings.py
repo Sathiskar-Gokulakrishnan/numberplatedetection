237a6f79663ebbf93f219d7b43071da9280c3b0e
Sathiskar-Gokulakrishnan <107302168+Sathiskar-Gokulakrishnan@users.noreply.github.com>
2023-11-07 21:03:09 +0530

    Allow host and debug set to false

--- number_plate_detection/settings.py
+++ number_plate_detection/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--4$vhj9oc#+==1$uy--l7!p589y&o4e$20fr71##zohw+zu)aq"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
